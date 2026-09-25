f7014e1cee9217297a27a9c9ab900f9722c97e31
yashhhh14 <yashwanthsunny4362@gmail.com>
2026-09-25 17:30:48 +0530

    url again changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://book-my-journeyv1.netlify.app"],
     methods: ["GET","POST","PUT","PATCH","DELETE"],
     credentials: true
 }));
